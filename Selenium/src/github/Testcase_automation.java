9f72b8720cbb157d5cce7d6a20d4707916def440
Sushmitha1311 <sushmithaacharya405@gmail.com>
2025-09-13 12:28:19 +0530

    Checkbox

--- Selenium/src/github/Testcase_automation.java
+++ Selenium/src/github/Testcase_automation.java
@@ -46,7 +46,6 @@ public class Testcase_automation {
                     && oBrowser.getTitle().equals("actiTIME - Login")
                     && oBrowser.findElement(By.xpath("//img[contains(@src, 'timer.png')]")).isDisplayed()){
                 System.out.println("logout from actiTime is successful");
-                System.out.println("--------------------------");
             }else{
                 System.out.println("Failed to logout from actiTime");
             }
