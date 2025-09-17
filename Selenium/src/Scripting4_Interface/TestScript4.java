d2b6ec8af82f9b10e1d20e8554bd6c8c699e13ad
Sushmitha1311 <sushmithaacharya405@gmail.com>
2025-09-17 11:12:08 +0530

    Scripting

---
+++ Selenium/src/Scripting4_Interface/TestScript4.java
@@ -0,0 +1,48 @@
+package Scripting4_Interface;
+
+import org.openqa.selenium.WebDriver;
+import java.util.Map;
+import static Scripting3_excel.ReusableMethods.getExcelData;
+import static Scripting4_Interface.ReusableMethod.*;
+
+public class TestScript4 {
+    public static void main(String[] args) {
+        TS_LoginLogout();
+        TS_createAndDeleteUser();
+    }
+    public static void TS_LoginLogout() {
+        WebDriver oBrowser = null;
+        Map<String, String> data = null;
+        try {
+            data=getExcelData("TestData","TestData","TC_ID_001");
+            oBrowser = launchBrowser(data.get("browserName"));
+            boolean blnRes = navigateURL(oBrowser, data.get("url"));
+            blnRes = loginToApplication(oBrowser, data.get("userName"), data.get("password"));
+            blnRes = logoutFromActiTime(oBrowser);
+        } catch (Exception e) {
+            System.out.println("Exception in 'TS_LoginLogout' test script");
+
+        } finally {
+            oBrowser.close();
+            oBrowser = null;
+        }
+    }
+    public static void TS_createAndDeleteUser() {
+        WebDriver oBrowser = null;
+        Map<String, String> data = null;
+        try {
+            data=getExcelData("TestData","TestData","TC_ID_002");
+            oBrowser = launchBrowser(data.get("browserName"));
+            boolean blnRes = navigateURL(oBrowser, data.get("url"));
+            blnRes = loginToApplication(oBrowser, data.get("userName"), data.get("password"));
+            String create = createUser(oBrowser, data);
+            boolean delete = deleteUser(oBrowser, create);
+            blnRes = logoutFromActiTime(oBrowser);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            oBrowser.close();
+            oBrowser = null;
+        }
+    }
+}
